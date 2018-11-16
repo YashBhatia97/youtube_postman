506d620abdd82ff65a31d4e517c1707247bf9540
YashBhatia97 <iit2015105@iiita.ac.in>
2018-11-16 19:37:11 +0530

    source build

---
+++ precache-manifest.874541c18d6c30b698d4c13b2240072c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a0a6a5ae421629557213",
+    "url": "/static/js/main.a0a6a5ae.chunk.js"
+  },
+  {
+    "revision": "5d43a7ea47706a455c4d",
+    "url": "/static/js/1.5d43a7ea.chunk.js"
+  },
+  {
+    "revision": "a0a6a5ae421629557213",
+    "url": "/static/css/main.b7089b2c.chunk.css"
+  },
+  {
+    "revision": "7c57b95eceb4a9689f5f6d10949efc1b",
+    "url": "/index.html"
+  }
+];
